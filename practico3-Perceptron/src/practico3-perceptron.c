9dcaa3f568d516111d97cdaf4c1ca306d5547cdb
Eric Nahuel Jurio <ericnahueljurio@gmail.com>
2011-08-12 05:23:17 +0000

    * Ejer2 casi listo (?)
    git-svn-id: https://redes-neuronales.googlecode.com/svn/trunk@18 9a5f7b75-e035-0c39-5710-0af1573a0d71

--- practico3-Perceptron/src/practico3-perceptron.c
+++ practico3-Perceptron/src/practico3-perceptron.c
@@ -74,6 +74,7 @@ int ejer1(int tamIn, int conMomento, int printRN) {
                 int resto = ((j * p) % totalCombinaciones);
                 if (totalCombinaciones <= resto + p)
                     mezclameLasCartas(inEnInt, totalCombinaciones, &sem);
+                red = rperc_destroy(red);
                 red = rperc_create(2, numNeuronas, tipoNuronas, NULL, NULL, &sem);
                 if (red) {
                     if (conMomento);
@@ -113,9 +114,9 @@ int ejer1(int tamIn, int conMomento, int printRN) {
 }
 
 int ejer2(int hypORln, int p, int conMomento, int printRN) {
-    int i = 0, j = 0, k = 0;
-    double **res = NULL, **in = NULL, *test = NULL, acum = 0.0, tot = 0.0;
-    long sem = -31;
+    int i = 0;
+    double **res = NULL, **in = NULL, *test = NULL;
+    long sem = -171;
     res = (double**) calloc(p, sizeof (double*));
     in = (double**) calloc(p, sizeof (double*));
     if (res && in) {
@@ -126,42 +127,57 @@ int ejer2(int hypORln, int p, int conMomento, int printRN) {
             in[i] = (double*) calloc(1, sizeof (double));
         }
         numNeuronas[0] = 1;
-        if (hypORln)
+        if (hypORln) {
             numNeuronas[1] = 4;
-        else
+            for (i = 0; i < p; i++) {
+                in[i][0] = 1.0 + ran2(&sem)*4.0;
+                res[i][0] = 1.0 / in[i][0];
+            }
+        } else {
             numNeuronas[1] = 6;
+            for (i = 0; i < p; i++) {
+                in[i][0] = 1.0 + ran2(&sem)*4.0;
+                res[i][0] = log(in[i][0]);
+            }
+        }
         numNeuronas[2] = 1;
         tipoNuronas[0] = 0;
         tipoNuronas[1] = 0;
         tipoNuronas[2] = 1;
-        /** /for (p = 5; p < 21; p *= 2) {*/
-        acum = 0.0;
-        tot = 0.0;
-        if (numNeuronas[0] && tipoNuronas[0]) printf("Estaaaa!!!\n");
-        /* Esto aca no hace falta deberia ser f(x) x[1;5] */
-        for (j = 0; j < 5000; j++) {
-            int numDeIntentos = 300;
-            red = rperc_create(2, numNeuronas, tipoNuronas, NULL, NULL, &sem);
-            if (red) {
-                double err = ((double) (numNeuronas[1] - 2)) / 200.0;
-                if (!conMomento) rperc_set_alfaMomento(red, .0);
-                rperc_set_rectaError(red, numDeIntentos, err, err, 0);
-                for (k = 0; k < p; k++)
-                    if (rperc_aprender(red, p, in, res, online)) {
-                        tot = tot + 1.0;
-                        test = rperc_eval(red, in[0]);
-                        if (dabs(res[0][0] - test[0]) < 1.0) acum = acum + 1.0;
-                        free(test);
-                        if (j == 4999 && printRN) {
-                            char *str = rperc_to_str(red);
-                            printf("%s\n", str);
-                            free(str);
-                        }
-                        red = rperc_destroy(red);
-                    } else if (numDeIntentos < 3100) numDeIntentos += 3;
-            } else printf("OO OOO\n");
-        }
-        /*}*/
+        red = rperc_create(2, numNeuronas, tipoNuronas, NULL, NULL, &sem);
+        if (red) {
+            double err = ((double) (numNeuronas[1] - 2)) / 200.0;
+            if (!conMomento) rperc_set_alfaMomento(red, .0);
+            rperc_set_rectaError(red, 1000, err, err, 0);
+            if (rperc_aprender(red, p, in, res, online)) {
+                printf("red(x): x in [1,5]: (solo %i puntos aleatorios)\n\n", p);
+                for (i = 0; i < p; i++) {
+                    test = rperc_eval(red, in[i]);
+                    printf("%f\t%f\n", in[i][0], test[0]);
+                    free(test);
+                }
+                printf("\nred(x): x in [1,5]:\n\n");
+                for (i = 0; i < p; i++) {
+                    double x = 1.0 + ((double) i)*.04;
+                    test = rperc_eval(red, &x);
+                    printf("%f\t%f\n", x, test[0]);
+                    free(test);
+                }
+                printf("\nf(x):\n\n");
+                for (i = 0; i < 100; i++) {
+                    double x = 1.0 + ((double) i)*.04;
+                    if (hypORln) printf("%f\t%f\n", x, 1.0 / x);
+                    else printf("%f\t%f\n", x, log(x));
+
+                }
+                if (printRN) {
+                    char *str = rperc_to_str(red);
+                    printf("%s\n\n\n", str);
+                    free(str);
+                }
+            } else printf("No aprendí :-S :'(  TT__TT\n");
+            red = rperc_destroy(red);
+        } else printf("OO OOO\n");
         for (i = 0; i < p; i++) {
             free(res[i]);
             free(in[i]);
@@ -177,7 +193,7 @@ int ejer3(int tamIn, int conMomento, int printRN) {
 }
 
 int main(int argc, char** argv) {
-    int i = 1, ejer = 0, todo_bien = 1, momento = 0, tam = -1, printRN = 0, cantMuest = 0;
+    int i = 1, ejer = 0, todo_bien = 1, momento = 0, tam = 1, printRN = 0, cantMuest = 5;
     while (todo_bien && i + 1 < argc) {
         if (!strcmp(argv[i], "-E") || !strcmp(argv[i], "-e")) {
             i++;
